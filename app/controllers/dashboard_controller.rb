4f5b2506f33ec2cb6d29b9567ecaaf5f0a613323
TMad <madsen.tyler@gmail.com>
2018-01-31 13:49:01 -0700

    build dashboard controller with API calls - needs refactoring

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,26 @@
 class DashboardController < ApplicationController
   def index
+    conn = Faraday.new(url: "https://api.github.com") do |faraday|
+      faraday.headers['Authorization'] = ["token #{current_user.token}"]
+      faraday.adapter Faraday.default_adapter
+    end
+    response = conn.get("/user/repos")
+    @user_repos = JSON.parse(response.body, symbolize_names: true).map do |repo|
+      Repository.new(repo)
+    end
+    response = conn.get("/users/#{current_user.username}/orgs")
+    @user_orgs = JSON.parse(response.body, symbolize_names: true)
+    response = conn.get("/users/#{current_user.username}/starred")
+    @user_starred = JSON.parse(response.body, symbolize_names: true)
+    response = conn.get("/users/#{current_user.username}/events")
+    @user_events = JSON.parse(response.body, symbolize_names: true).map do |event|
+      if event[:type] == "PushEvent"
+        PushEvent.new(event)
+      elsif event[:type] == "CreateEvent"
+        CreateEvent.new(event)
+      elsif event[:type] == "PullRequestEvent"
+        PullRequestEvent.new(event)
+      end
+    end
   end
 end

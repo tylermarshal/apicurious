e3a0fb9510fec3bc6f60a8b2f4f7be9031421032
TMad <madsen.tyler@gmail.com>
2018-01-31 15:17:21 -0700

    update repos call in dashboard controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,13 +1,6 @@
 class DashboardController < ApplicationController
   def index
-    conn = Faraday.new(url: "https://api.github.com") do |faraday|
-      faraday.headers['Authorization'] = ["token #{current_user.token}"]
-      faraday.adapter Faraday.default_adapter
-    end
-    response = conn.get("/user/repos")
-    @user_repos = JSON.parse(response.body, symbolize_names: true).map do |repo|
-      Repository.new(repo)
-    end
+    @user_repos = RepoService.new(current_user).create_repos
     @user_orgs = GithubService.new(current_user).orgs
     @user_starred = GithubService.new(current_user).starred
     @user_events = EventService.new(current_user).formatted_events

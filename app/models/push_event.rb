88d2be1b56b69cd5e06fb47eda149d50510183cc
TMad <madsen.tyler@gmail.com>
2018-01-31 15:02:41 -0700

    add create commit method for model

--- app/models/push_event.rb
+++ app/models/push_event.rb
@@ -1,17 +1,17 @@
 class PushEvent
 
-  attr_reader :repo, :public_status, :commit_urls
+  attr_reader :repo, :public_status, :commit_urls, :current_user
 
-  def initialize(event)
+  def initialize(event, current_user)
     @repo = event[:repo][:name]
     @public_status = event[:public]
     @commit_urls = event[:payload][:commits].map {|commit| commit[:url]}
+    @current_user = current_user
   end
 
   def create_commits
-    commit_urls.each do |commit|
-      response = conn.get("#{commit}")
-      commit_event = JSON.parse(response.body, symbolize_names: true)
+    commit_urls.map do |commit_url|
+      commit_event = GithubService.new(current_user).commit_conn(commit_url)
       Commit.new(repo, public_status, commit_event)
     end
   end

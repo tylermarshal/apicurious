80caa101bcff1e736185090be8613b7f948a25b1
TMad <madsen.tyler@gmail.com>
2018-01-31 13:48:32 -0700

    add user model with caching strategy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,36 @@
 class User < ApplicationRecord
 
   def self.from_omniauth(auth_info)
-    where(uid: auth_info[:uid]).first_or_create do |new_user|
-      new_user.uid                = auth_info.uid
-      new_user.name               = auth_info.extra.raw_info.name
-      new_user.username           = auth_info.extra.raw_info.username
-      new_user.company            = auth_info.extra.raw_info.company
-      new_user.bio                = auth_info.extra.raw_info.bio
-      new_user.email              = auth_info.extra.raw_info.email
-      new_user.token              = auth_info.credentials.token
+    user = find_by(uid: auth_info[:uid])
+    if user.nil?
+      create do |new_user|
+        self.create_or_update(new_user, auth_info)
+      end
+    else
+      if user.last_updated <= auth_info.extra.raw_info.updated_at
+        user
+      else
+        self.create_or_update(user, auth_info)
+      end
+    end
+  end
+
+  def self.create_or_update(user_type, auth_info)
+    user_type.uid                = auth_info.uid
+    user_type.name               = auth_info.extra.raw_info.name
+    user_type.username           = auth_info.extra.raw_info.login
+    user_type.avatar             = auth_info.extra.raw_info.avatar_url
+    user_type.followers          = auth_info.extra.raw_info.followers
+    user_type.following          = auth_info.extra.raw_info.following
+    user_type.public_repos       = auth_info.extra.raw_info.public_repos
+    user_type.company            = auth_info.extra.raw_info.company
+    user_type.location           = auth_info.extra.raw_info.location
+    user_type.bio                = auth_info.extra.raw_info.bio
+    user_type.email              = auth_info.extra.raw_info.email
+    user_type.last_updated       = auth_info.extra.raw_info.updated_at
+    user_type.token              = auth_info.credentials.token
+    if user_type == user
+      user.save!
     end
   end
 
